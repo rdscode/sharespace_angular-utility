f78b727d6ceb904f298f2b848e3a8d555ef27b12
Sampath Randeniya <sampath.randeniya@abstraction.co>
2015-10-26 13:15:49 +0530

    bxslider config

--- directives/bxSlider.js
+++ directives/bxSlider.js
@@ -4,49 +4,45 @@
 
 angular.module('tgwcAngularUtility')
     .directive('wrapBxSlider', function () {
-        var linker = function(scope,element,attr){
+        alert();
+        return {
+            link: function (scope, elem, attr) {
 
-            var loadBxSlider = function(){
-                //var options = scope.$eval($(element).attr('data-options'));
-                //element.owlCarousel(options);
-            };
+                scope.slider = angular.element('.bxslider');
 
-            var loadBxSliderActions = function(){
+                //$(document).ready(function () {
+                //
+                //    scope.slider = angular.element('.bxslider');
+                //
+                //    scope.slider.bxSlider({
+                //        adaptiveHeight: true,
+                //        pagerCustom: '#owl-demo'
+                //    });
+                //
+                //});
 
-                scope.slider = $('.bxslider').bxSlider();
+                scope.slider = angular.element('.bxslider').bxSlider();
 
-                angular.element(".bxslider_next").click(function(){
-                    alert()
+                angular.element(".bxslider_next").click(function () {
                     scope.slider.goToNextSlide();
                 });
-                angular.element(".bxslider_prev").click(function(){
+                angular.element(".bxslider_prev").click(function () {
                     scope.slider.goToPrevSlide();
                 });
-                angular.element(".bxslider_play").click(function(){
+                angular.element(".bxslider_play").click(function () {
                     scope.slider.startAuto();
                 });
-                angular.element(".bxslider_stop").click(function(){
+                angular.element(".bxslider_stop").click(function () {
                     scope.slider.stopAuto();
                 });
-                angular.element(".bxslider_reload").click(function(){
+                angular.element(".bxslider_reload").click(function () {
                     scope.slider.reloadSlider();
                 });
-                angular.element(".bxslider_destroy").click(function(){
+                angular.element(".bxslider_destroy").click(function () {
                     scope.slider.destroySlider();
                 });
-            };
 
-            //scope.$watch(element.attr("render-watch"), function(value) {
-            //    if (typeof value != 'undefined') {
-            //        loadBxSlider(element);
-            //    }
-            //});
-
-            loadBxSliderActions();
-        };
-
-        return{
-            restrict : "EA",
-            link: linker
+            }
         }
+
     });
