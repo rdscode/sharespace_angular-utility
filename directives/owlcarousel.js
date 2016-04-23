0c1a9aec8f95c7410fb2ed7d530a478418438d67
yaseersheriff <yaseer.sheriff@gmail.com>
2016-04-23 19:15:57 +0000

    Added missing function

--- directives/owlcarousel.js
+++ directives/owlcarousel.js
@@ -14,8 +14,28 @@ angular.module('tgwcAngularUtility')
         var options = scope.$eval($(element).attr('data-options'));
         element.owlCarousel(options);
       };
+      
+     var loadCarouselActions = function(){
+      console.log('adding actions');
+      angular.element(".owlcarousel_next").click(function(){
+        angular.element(".owlcarousel_next").attr("href", "");
+        element.trigger('owl.next');
+      });
+      angular.element(".owlcarousel_prev").click(function(){
+        angular.element(".owlcarousel_prev").attr("href", "");
+        element.trigger('owl.prev');
+      });
+      angular.element(".owlcarousel_play").click(function(){
+        angular.element(".owlcarousel_play").attr("href", "");
+        element.trigger('owl.play', 1000);
+      });
+      angular.element(".owlcarousel_stop").click(function(){
+        angular.element(".owlcarousel_stop").attr("href", "");
+        element.trigger('owl.stop');
+      })
+    };
 
-      scope.$watch(element.attr("render-watch"), function(value) {
+    scope.$watch(element.attr("render-watch"), function(value) {
         if (typeof value != 'undefined') {
           loadCarousel(element);
           loadCarouselActions();
